0b11ad9ceb31fda15e22148887fce1f032f3256d
Harold Tai <harodtai321@gmail.com>
2022-03-24 22:35:18 +0800

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <nav className="topNav">
+        <h1>Navbar content</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/countryData.js
@@ -0,0 +1,31 @@
+const countryData = [
+    {
+        "title": "St Vitus Cathedral",
+        "location": "Europe",
+        "googleMapsUrl": "https://www.google.com/maps/place/St.+Vitus+Cathedral/@50.0908918,14.3983227,17z/data=!3m1!4b1!4m5!3m4!1s0x470b951e38024f5b:0x69ad3850f2d989a2!8m2!3d50.0908918!4d14.4005114?hl=en",
+        "startDate:": "19 Nov, 2019",
+        "endDate": "28 Nov, 2019",
+        "description": "The Metropolitan Cathedral of Saints Vitus, Wenceslaus and Adalbert is a Roman Catholic metropolitan cathedral in Prague, the seat of the Archbishop of Prague",
+        "imageUrl": require("./images/stVitus.jpg")
+    },
+    {
+        "title": "Namsangol Hanok Village",
+        "location": "South Korea",
+        "googleMapsUrl": "https://www.google.com/maps/place/Namsangol+Hanok+Village,+Seoul/@37.559315,126.9922883,17z/data=!3m1!4b1!4m5!3m4!1s0x357ca2fcfa6a299b:0x79c3be57e1edbdfe!8m2!3d37.559315!4d126.994477",
+        "startDate:": "10 Dec, 2017",
+        "endDate": "17 Dec, 2017",
+        "description": "Namsangol Hanok Village, also known as 'A Village of Traditional Houses in the Namsan Valley', is a Korean village located in the area of Pil-dong neighborhood in Jung-gu, a central district of Seoul, South Korea where hanok (한옥) or Korean traditional houses have been restored to preserve the original atmosphere of the area.",
+        "imageUrl": require("./images/namsangol.jpg")
+    },
+    {
+        "title": "Old Changi Hospital",
+        "location": "Singapore",
+        "googleMapsUrl": "https://www.google.com/maps/place/Former+Changi+Hospital/@1.3902775,103.9781961,17z/data=!3m1!4b1!4m5!3m4!1s0x31da3c3cd1ebdf1b:0x36fface1678acb92!8m2!3d1.3902775!4d103.9803848",
+        "startDate:": "12 May, 2017",
+        "endDate": "12 May, 2017",
+        "description": "The former Changi Hospital is a hospital in Singapore, the oldest hospital in Singapore, and the first hospital in Singapore to be built in the late 19th century.",
+        "imageUrl": require("./images/oldChangi.jpg")
+    }
+]
+
+export default countryData;
